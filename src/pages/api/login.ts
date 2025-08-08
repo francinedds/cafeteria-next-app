fe7b4b554dbaaa1cb1fea535b10e367351936036
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-08-08 13:01:17 +0000

    Update: add new data to login API response

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -7,15 +7,27 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const { email, password } = req.body;
 
-  // Login fixo (sem banco)
+  // Usuário mockado (sem banco)
   const fakeUser = {
     email: 'admin@cafeteria.com',
     password: '123',
+    user: 'Administrador',
+    phone: '(00) 00000-0000',
+    ocupation: 'Dono da cafeteria'
+    
   };
 
   if (email === fakeUser.email && password === fakeUser.password) {
-    return res.status(200).json({ message: 'Login bem-sucedido' });
+    return res.status(200).json({
+      message: 'Login bem-sucedido!',
+      user: {
+        email: fakeUser.email,
+        user: fakeUser.user,
+        phone: fakeUser.phone,
+        ocupation: fakeUser.ocupation
+      }
+    });
   }
 
-  return res.status(401).json({ message: 'Email ou senha incorretos' });
+  return res.status(401).json({ message: 'Email ou senha incorretos!' });
 }
