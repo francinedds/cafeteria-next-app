3e91cb2c4435f5f3c91186ca8b9e565445a084f0
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-08-08 13:08:05 +0000

    Feat: add toast notification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
 import { BagProvider } from "@/context/BagContext";
+import { ToastContainer } from "react-toastify";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,5 +16,10 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return <BagProvider>{getLayout(<Component {...pageProps} />)}</BagProvider>;
+  return (
+    <> 
+      <BagProvider>{getLayout(<Component {...pageProps} />)}</BagProvider>
+      <ToastContainer position="top-right" autoClose={3000} /> {/* parte modificada */}
+    </>
+  ); 
 }
