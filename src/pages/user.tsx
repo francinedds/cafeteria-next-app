64f69c9a3f38e7cc17a6beacdd9d306559dfec48
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-08-08 12:59:02 +0000

    Update: Page User

---
+++ src/pages/user.tsx
@@ -0,0 +1,98 @@
+import { ReactElement, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { CaretLeftIcon, SignOutIcon } from '@phosphor-icons/react';
+
+import * as Avatar from '@radix-ui/react-avatar';
+
+import Layout from '@/components/Layout';
+
+type User = {
+  email: string;
+  user?: string;
+  phone?: string;
+  avatarUrl?: string;
+};
+
+export default function User() {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const userData = localStorage.getItem('user');
+    if (!userData) {
+      router.push('/login');
+    } else {
+      setUser(JSON.parse(userData));
+    }
+  }, [router]);
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    router.push('/login');
+  };
+
+  if (!user) return <p>Carregando...</p>;
+
+  return (
+    <>
+      <header className="relative flex items-center w-full h-15 bg-white p-4">
+      <button onClick={() => router.back()} aria-label="Voltar">
+        <CaretLeftIcon size={28} />
+      </button>
+
+      <h2 className="absolute left-1/2 transform -translate-x-1/2 font-semibold">
+        MEU PERFIL
+      </h2>
+    </header>
+      <div className="pb-24 flex flex-col items-center justify-center min-h-screen">
+        <div className="bg-white shadow-lg rounded-xl p-8 w-[300px] flex flex-col items-center">
+          <Avatar.Root
+            style={{
+              display: 'inline-flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: '100%',
+              width: 100,
+              height: 100,
+              backgroundColor: '#4E331B',
+              overflow: 'hidden',
+              userSelect: 'none',
+            }}
+          >
+            {user.avatarUrl ? (
+              <Avatar.Image
+                src={user.avatarUrl}
+                alt={`${user.user || 'Usuário'} avatar`}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            ) : (
+              <Avatar.Fallback
+                delayMs={600}
+                style={{
+                  color: '#fff',
+                  fontSize: 40,
+                  fontWeight: 'bold',
+                  userSelect: 'none',
+                }}
+              >
+                {(user.user || user.email)[0].toUpperCase()}
+              </Avatar.Fallback>
+            )}
+          </Avatar.Root>
+
+          <h2 className="mt-4 mb-2 font-semibold">Bem-vindo, {user.user}</h2>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          <button onClick={logout} className="mt-6 flex flex-row">
+            <SignOutIcon size={24} />
+            <span className="ml-2">Sair</span> 
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+User.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
